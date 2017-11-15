84ce984937acfac65e8d61e3c400ff78108f7a5d
atm-ph <phf@atm-consulting.Fr>
2017-11-15 12:02:21 +0100

    Fix tk6734 - sql group

--- class/actions_facturerreception.class.php
+++ class/actions_facturerreception.class.php
@@ -112,7 +112,7 @@ class ActionsfacturerReception
 				$resultset = $db->query("SELECT DATE_FORMAT(datec,'%Y-%m-%d %H:%i:00') as 'date', datec as 'datem', SUM(qty) as 'nb'
 				FROM ".MAIN_DB_PREFIX."commande_fournisseur_dispatch 
 				WHERE fk_commande=".$object->id
-				." GROUP BY date ");
+				." GROUP BY date, datem ");
 
 				if ($resultset)
 				{
