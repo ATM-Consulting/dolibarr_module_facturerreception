39d8225729869de527b9cbcf2d1ec15eeb560c98
gauthier <gauthier.verdol@atm-consulting.fr>
2019-01-29 10:10:07 +0100

    FIX : nouvelle méthode également gérée quand on facture une réception depuis la fiche commande

--- class/actions_facturerreception.class.php
+++ class/actions_facturerreception.class.php
@@ -73,7 +73,7 @@ class ActionsfacturerReception
 				$datereception = GETPOST('datereception');
 				
 				if(!empty($datereception)) {
-					$resultset = $db->query("SELECT fk_commandefourndet,fk_product,SUM(qty) as qty
+					$resultset = $db->query("SELECT GROUP_CONCAT(rowid) as TRowid, fk_commandefourndet,fk_product,SUM(qty) as qty
 					FROM ".MAIN_DB_PREFIX."commande_fournisseur_dispatch 
 					WHERE fk_commande=".$object->id."
 					AND datec LIKE '".date('Y-m-d H', strtotime($datereception))."%'
@@ -84,7 +84,6 @@ class ActionsfacturerReception
 					$Tab = array();
 					while($obj = $db->fetch_object($resultset)) {
 						$obj->line = getGoodLine($object, $obj->fk_commandefourndet, $obj->fk_product);
-						
 						$Tab[] = $obj;
 					}
 					
@@ -109,10 +108,11 @@ class ActionsfacturerReception
 		{
 			if ($user->rights->fournisseur->facture->creer)
 			{
-				$resultset = $db->query("SELECT DATE_FORMAT(datec,'%Y-%m-%d %H:%i:00') as 'date', datec as 'datem', SUM(qty) as 'nb'
-				FROM ".MAIN_DB_PREFIX."commande_fournisseur_dispatch 
-				WHERE fk_commande=".$object->id
-				." GROUP BY date, datec ");
+				$resultset = $db->query("SELECT DATE_FORMAT(cfd.datec,'%Y-%m-%d %H:%i:00') as 'date', cfd.datec as 'datem', SUM(cfd.qty) as 'nb'
+				FROM ".MAIN_DB_PREFIX."commande_fournisseur_dispatch cfd
+				LEFT JOIN ".MAIN_DB_PREFIX."element_element ee ON (ee.sourcetype='commandefournisseurdispatch' AND ee.fk_source=cfd.rowid)
+				WHERE cfd.fk_commande=".$object->id
+				." GROUP BY date, datec HAVING GROUP_CONCAT(ee.fk_source) is null");
 
 				if ($resultset)
 				{
